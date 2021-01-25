3e069eef89d09695ff298615b488e4a75f849721
MANISH MOTWANI <59482117+manishmotwani2002@users.noreply.github.com>
2021-01-25 18:00:49 +0530

    Update rod cutting.cpp

--- day 15 DP/rod cutting.cpp
+++ day 15 DP/rod cutting.cpp
@@ -1,17 +1,49 @@
-//solution is referred through GFG
+#include<bits/stdc++.h>
+using namespace std;
 
-int maximizeTheCuts(int n, int a, int b, int c)
-{
-    int dp[n+1];
-    dp[0]=0;
+int maximizeTheCuts(int n, int x, int y, int z);
+
+int main() {
     
-    for(int i=1;i<=n;i++)
+    //taking testcases
+    int t;
+    cin >> t;
+    while(t--)
     {
-        dp[i]=-1;
-        if(i-a>=0) dp[i]=max(dp[i],dp[i-a]);
-        if(i-b>=0) dp[i]=max(dp[i],dp[i-b]);
-        if(i-c>=0) dp[i]=max(dp[i],dp[i-c]);
-        if(dp[i]!=-1) dp[i]++;
+        //taking length of line segment
+        int n;
+        cin >> n;
+        
+        //taking types of segments
+        int x,y,z;
+        cin>>x>>y>>z;
+        
+        //calling function maximizeTheCuts()
+        cout<<maximizeTheCuts(n,x,y,z)<<endl;
+
     }
-    return max(dp[n],0);
+
+	return 0;
+}// } Driver Code Ends
+
+int dp[10004];
+//Complete this function
+int maxcut(int n,int x,int y,int z){
+if(n==0)
+return 0;
+else if(n<0)
+return INT_MIN;
+else if(dp[n]!=-1)
+return dp[n];
+else
+return dp[n]=max(1+maxcut(n-x,x,y,z),max(1+maxcut(n-y,x,y,z),1+maxcut(n-z,x,y,z)));
+}
+int maximizeTheCuts(int n, int x, int y, int z)
+{
+memset(dp,-1,sizeof(dp));
+int a= maxcut(n,x,y,z);
+if(a<0)
+return 0;
+else
+return a;
 }
